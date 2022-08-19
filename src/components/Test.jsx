3c31b00c9a267446db79bbd26a0939d9d74d8cd2
MaiVietCuong <s3682365@rmit.edu.vn>
2022-08-19 09:57:55 +0700

    test event message

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 
 const CHATWOOT_API =
@@ -7,24 +8,9 @@ const Test = () => {
   const [currentContact, setCurrentContact] = useState({})
 
   useEffect(() => {
-    fetch(CHATWOOT_API, {
-      headers: {
-        Accept: 'application/json',
-        api_access_token: 'yVhhy9ja14JHeTQznz9qri6g',
-      },
+    window.addEventListener('message', function (event) {
+      console.log(event)
     })
-      .then((response) => response.json())
-      .then((data) => console.log(data))
-    // window.addEventListener('click', function () {
-    //   const url = this.window.location.href
-
-    //   //   if (!url.startsWith('http://127.0.0.1:5173/')) return false
-
-    //   const contact_ID = url.toString().substring(url.lastIndexOf('/') + 1)
-
-    //   getUser()
-    //   //   getConversationDetail().then((data) => console.log(data))
-    // })
   }, [])
 
   return <div>Test</div>
